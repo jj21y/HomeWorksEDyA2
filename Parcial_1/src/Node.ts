cd194b4f4f554dcc5da03021632cd965026fa758
juan_josn.bernal <juan_josn.bernal@uao.edu.co>
2026-03-04 16:38:06 -0500

    updte 1

---
+++ Parcial_1/src/Node.ts
@@ -0,0 +1,12 @@
+
+class Node {
+    value: string 
+    next: null
+    prev: null
+    constructor(value: string){
+        this.value = value
+        this.next = null;
+        this.prev = null;
+    }
+}
+export default Node;
