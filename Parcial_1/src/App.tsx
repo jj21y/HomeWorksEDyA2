0ff2019709d9eac5b1004398cdfe56315fe6ff49
juan_josn.bernal <juan_josn.bernal@uao.edu.co>
2026-03-04 16:37:35 -0500

    update 2

--- Parcial_1/src/App.tsx
+++ Parcial_1/src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+
 import './App.css'
+  type Patient = {
+    nombre: string;
+    cedula: string;
+    next: Patient | null;
+  }
 
+  type History = {
+    nombre: string;
+    cedula: string;
+    prev: History | null;
+    next: History | null;
+  }
+
+  type Doctor = {
+    nombre: string;
+    codigo: string;
+    next: Doctor | null;
+  }
+
+  type MiembroComite = {
+    nombre: string;
+    numeroMiembro: string;
+    next: MiembroComite | null;
+    prev: MiembroComite | null;
+  }
 function App() {
-  const [count, setCount] = useState(0)
+  const [HeadPatient, setHeadPatient] = useState<Patient | null>(null);
+  const [NombrePatient, setNombrePatient] = useState("");
+  const [CedulaPatient, setCedulaPatient] = useState("");
+  const [HeadHistory, setHeadHistory] = useState<History | null>(null);
+  const [DoctorActual, setDoctorActual] = useState <Doctor | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+  const addpatient = () => {
+    if (!NombrePatient || !CedulaPatient)
+      return "Error no se pudo agregar al Paciente";
+    const nuevo: Patient = {
+      nombre: NombrePatient,
+      cedula: CedulaPatient,
+      next: null
+    }
+     if (!HeadPatient) {
+      setHeadPatient(nuevo);
+    } else {
+      let actual = HeadPatient;
+      while (actual.next) {
+        actual = actual.next;
+      }
+      actual.next = nuevo;
+    }
+
+    setNombrePatient("");
+    setCedulaPatient("");
+  }
+  const addHistory = (nombre: string, cedula: string) => {
+    const nuevo: History = {
+      nombre,
+      cedula,
+      prev: null,
+      next: null
+    }
+    if(!HeadHistory){
+      setHeadHistory(nuevo)
+    } else {
+      let actual = HeadHistory
+      while (actual.next){
+        actual = actual.next;
+      }
+      actual.next = nuevo;
+    }
+  }
+  const atenderPatient = () => {
+    if(!HeadPatient)
+      return "No se puede atender al paciente";
+
+    const nombre = HeadPatient.nombre;
+    const cedula = HeadPatient.cedula;
+
+    addHistory(nombre, cedula)
+    setHeadPatient(HeadPatient.next)
+  }
+  
+  useEffect(() => {
+    const D1: Doctor = {nombre: "Doc Juan Jose", codigo: "2234991", next: null}
+    const D2: Doctor = {nombre: "Doc Ana Sofia", codigo: "2234881", next: null}
+    const D3: Doctor = {nombre: "Doc Lucy", codigo: "2234771", next: null}
+  })
+
+
 }
+
 
 export default App
