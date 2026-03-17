2fb180cf669874687230b08fb2514fe15b334c9f
juan_josn.bernal <juan_josn.bernal@uao.edu.co>
2026-03-17 13:53:34 -0500

    Stack class

---
+++ Challenge04/src/Stack.ts
@@ -0,0 +1,24 @@
+class Stack <T> {
+    private items: T [] = [];
+
+    push(element: T){
+        this.items.push(element)
+    }
+    pop(): T | undefined {
+        return this.items.pop();
+    }
+    peek(): T | undefined {
+        return this.items[this.items.length -1 ]
+    }
+    isEmpty(): boolean {
+        return this.items.length === 0
+    }
+    size(): number {
+        return this.items.length
+    }
+    print(): T[] {
+        return this.items;
+    }
+}
+
+export default Stack;
