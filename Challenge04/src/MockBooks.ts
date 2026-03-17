ce506db818f5fffc7bcc1e1a101d9bae4c52fb42
juan_josn.bernal <juan_josn.bernal@uao.edu.co>
2026-03-17 13:06:36 -0500

    Estructura y Stock de libros

---
+++ Challenge04/src/MockBooks.ts
@@ -0,0 +1,36 @@
+import {Book} from 'src/Book.ts';
+
+const MockBooks: Books[] = [
+    {
+        name: "The Lord of The Rings: The FellowShip of The Ring",
+        isbn: "9780007136599",
+        author: "J.R.Tolkien",
+        editorial: "George Allen & Unwin"
+    },
+    {
+        name: "The Lord of The Rings: The Two Towers",
+        isbn: "9788845292613",
+        author: "J.R.Tolkien",
+        editorial: "George Allen & Unwin"
+    },
+    {
+        name: "The Lord of The Rings: The Return of the King",
+        isbn: "9780007136575",
+        author: "J.R.Tolkien",
+        editorial: "George Allen & Unwin"
+    },
+    {
+        name: "Little Prince",
+        isbn: "9780152023980",
+        author: " Antoine De Saint Exupery",
+        editorial: "Clarion Books"
+    },
+    {
+        name: "Cien Años de Soledad",
+        isbn: "9788437604947",
+        author: "Gabriel Garcia Marquez ",
+        editorial: "Debolsillo"
+    }
+];
+
+export default MockBooks;
