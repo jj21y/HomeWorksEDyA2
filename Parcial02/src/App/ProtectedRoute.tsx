cdbb936626c538cd9c19ac7b5ba1316eadd897a7
Juan Jose <juan_josn.bernal@uao.edu.co>
2026-04-15 15:40:11 -0500

    fix?

--- Parcial02/src/App/ProtectedRoute.tsx
+++ Parcial02/src/App/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { useAuth } from "../Context/AuthContext";
 
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const { user } = useAuth();
-
+  
   if (!user) return <Navigate to="/login" replace />;
 
   return children;
